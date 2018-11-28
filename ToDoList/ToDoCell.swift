5da95dc5f7fc6e2708d04735d769ffb3d4a0f637
bartwitting <bartwitting@icloud.com>
2018-11-28 13:09:21 +0100

    Bijna af

--- ToDoList/ToDoCell.swift
+++ ToDoList/ToDoCell.swift
@@ -21,16 +21,5 @@ class ToDoCell: UITableViewCell {
     @IBAction func isCompletePressed(_ sender: UIButton) {
         delegate?.checkmarkTapped(sender: self)
     }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
